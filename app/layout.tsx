dddf5b4e15779750d91897e785564c0d767df8be
kalio007 <kalioprince1@gmail.com>
2024-08-03 19:19:58 +0100

    modified pnpm-lock file

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Analytics } from "@/components/analytics";
 import { ModeToggle } from "@/components/mode-toggle";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Leo By Example",
@@ -20,7 +20,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body
-        className={`antialiased min-h-screen bg-[#F5F5F5] dark:bg-[#121212] text-slate-900 dark:text-slate-50 ${inter.className}`}
+        className={`antialiased min-h-screen bg-[#F5F5F5] dark:bg-[#121212] text-slate-900 dark:text-slate-50 `}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="max-w-2xl mx-auto py-10 px-4">
